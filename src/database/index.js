c89a221124e7169830785e19990b3075192029a1
Promise <me@promise.solutions>
2021-08-02 22:34:12 +0200

    Move autoresponses to mongoose

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,11 @@
 module.exports = {
+  // local
   emojis: require("./local")("emojis"),
   quickresponses: require("./local")("quickresponses"),
   reactionroles: require("./local")("reactionroles"),
   shardstatus: require("./local")("shardstatus"),
   tickets: require("./local")("tickets"),
   botMonitor: require("./local")("botMonitor"),
+  // mongo
+  mlTraining: require("./mongo/trainingModel")
 };
---
+++ src/database/mongo/trainingModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const trainingModelSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  data: { type: Map, of: {
+    input: { type: [String], default: [] },
+    quickresponse: { type: String, required: true },
+    confidenceRequired: { type: Number, default: 0.5 }
+  }, default: [] }
+});
+
+const TrainingModel = mongoose.model("TrainingModel", trainingModelSchema);
+
+module.exports = TrainingModel;
+
+TrainingModel.find().then(console.log);
